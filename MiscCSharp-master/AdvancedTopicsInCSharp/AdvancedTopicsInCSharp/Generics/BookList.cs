acec78eb5700fb9d9f5141f8c5e89b7ace83f56e
Johnathan R. Burgess <ryanburgess173@outlook.com>
2024-01-24 10:49:16 -0500

    Add files via upload

---
+++ MiscCSharp-master/AdvancedTopicsInCSharp/AdvancedTopicsInCSharp/Generics/BookList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace AdvancedTopicsInCSharp.Generics
+{
+	public class BookList
+	{
+		public void Add(Book book)
+		{
+			throw new NotImplementedException();
+		}
+		public Book this[int index]
+		{
+			get { throw new NotImplementedException(); }
+		}
+	}
+	// There's 2 performance penalties associated with this solution for
+	// multiple data types:
+	// 1) when you want to pass an object into one of these class methods,
+	//	  you're paying for the performance cost of boxing and unboxing it.
+	// 2) casting say an integer to an object and object to an integer is
+	//	  also costly.
+	public class ObjectList
+	{
+		public void Add(object value)
+		{
+
+		}
+		public object this[int index]
+		{
+			get { throw new NotImplementedException(); }
+		}
+	}
+
+	public class GenericDictionary<TKey, TValue>
+	{
+		public void Add(TKey key, TValue value)
+		{
+
+		}
+	}
+
+	public class GenericList<T>
+	{
+		public void Add(T value)
+		{
+
+		}
+		public T this[int index]
+		{
+			get { throw new NotImplementedException(); }
+		}
+	}
+}
+
---
+++ MiscCSharp-master/AdvancedTopicsInCSharp/AdvancedTopicsInCSharp/Generics/Nullable.cs
@@ -0,0 +1,25 @@
+﻿namespace Generics
+{
+    public class Nullable<T> where T : struct
+    {
+        public object _value { get; set; }
+        public Nullable(T value)
+        {
+            _value = value;
+        }
+
+        public bool HasValue
+        {
+            get { return _value != null; }
+        }
+
+        public T GetValueOrDefault()
+        {
+            if (HasValue)
+            {
+                return (T)_value;
+            }
+            return default(T);
+        }
+    }
+}
