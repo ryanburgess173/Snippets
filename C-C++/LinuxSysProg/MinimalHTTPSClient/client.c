3f23486a49a1245ea1e42ad0c166ca22b21550e7
Ryan Burgess <ryanburgess173@outlook.com>
2025-11-16 14:46:08 -0500

    HTTP client in C  written

--- C-C++/LinuxSysProg/MinimalHTTPSClient/client.c
+++ C-C++/LinuxSysProg/MinimalHTTPSClient/client.c
@@ -1,6 +1,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <openssl/ssl.h>
 
 void main(){
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -17,4 +20,12 @@ void main(){
 	SSL* ssl = SSL_new(ctx);
 	SSL_set_fd(ssl, sockfd);
 	SSL_connect(ssl);
+
+	char* request = "GET /\r\n\r\n";
+	SSL_write(ssl, request, strlen(request));
+
+	char buffer[1024] = {0};
+	SSL_read(ssl, buffer, 1023);
+
+	printf("Response:\n%s\n", buffer);
 }
